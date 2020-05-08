4259c00b67484fcec1abc5ce29636915549dbe2c
15bce102 <rohit102mittal@gmail.com>
2020-05-08 12:56:15 +0530

    Dynamic link added

--- App/app/src/main/java/com/example/polify/MainActivity.java
+++ App/app/src/main/java/com/example/polify/MainActivity.java
@@ -1,40 +1,49 @@
 package com.example.polify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 import com.example.polify.ui.activity.HomeActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.dynamiclinks.DynamicLink;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MAinActivity";
     private EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        handleDynamicLinks();
+
         editText = findViewById(R.id.phoneNumber);
-        findViewById(R.id.sendOtp).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String number = editText.getText().toString().trim();
-                if (number.isEmpty() || number.length() < 10) {
-                    editText.setError("Valid number is required");
-                    editText.requestFocus();
-                    return;
-                }
-
-                String phoneNumber = "+" + "91" + number;
-
-                Intent intent = new Intent(MainActivity.this, VerifyActivity.class);
-                intent.putExtra("phoneNumber", phoneNumber);
-                startActivity(intent);
+        findViewById(R.id.sendOtp).setOnClickListener(v -> {
+            String number = editText.getText().toString().trim();
+            if (number.isEmpty() || number.length() < 10) {
+                editText.setError("Valid number is required");
+                editText.requestFocus();
+                return;
             }
+
+            String phoneNumber = "+" + "91" + number;
+
+            Intent intent = new Intent(MainActivity.this, VerifyActivity.class);
+            intent.putExtra("phoneNumber", phoneNumber);
+            startActivity(intent);
         });
     }
 
@@ -48,5 +57,18 @@ public class MainActivity extends AppCompatActivity {
 
             startActivity(intent);
         }
+    }
+
+    public void handleDynamicLinks(){
+        FirebaseDynamicLinks.getInstance()
+                .getDynamicLink(getIntent())
+                .addOnSuccessListener(this, pendingDynamicLinkData -> {
+                    Uri deepLink = null;
+                    if(pendingDynamicLinkData!=null){
+                        deepLink = pendingDynamicLinkData.getLink();
+                    }
+
+                })
+                .addOnFailureListener(this, e -> Log.d(TAG,"Dynamic link failure"));
     }
 }
