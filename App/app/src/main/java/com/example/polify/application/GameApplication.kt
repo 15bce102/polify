7ef32d791a6179063f53d5ff14a8382a8b93a930
DeaDshot14 <mistry14darren@gmail.com>
2020-05-15 01:23:05 +0530

    update user status every 2 min

---
+++ App/app/src/main/java/com/example/polify/application/GameApplication.kt
@@ -0,0 +1,4 @@
+package com.example.polify.application
+
+class GameApplication {
+}
---
+++ App/app/src/main/java/com/example/polify/service/StatusUpdateService.kt
@@ -0,0 +1,12 @@
+package com.example.polify.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class StatusUpdateService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
